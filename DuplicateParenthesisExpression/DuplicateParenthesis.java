341498dadcdae7862198c73866f36163cdd98fa0
Elija33 <amponsaeli@gmail.com>
2022-11-24 01:45:43 -0500

    Post fix Expression

--- DuplicateParenthesisExpression/DuplicateParenthesis.java
+++ DuplicateParenthesisExpression/DuplicateParenthesis.java
@@ -5,17 +5,43 @@ import java.util.Deque;
 
 public class DuplicateParenthesis {
     public static boolean Duplicate(String expression) {
+
+        // create a stack of characters
         Deque<Character> word = new ArrayDeque<>();
-        for (int i = 0; i < expression.length(); i++) {
-            char checking = expression.charAt(i);
-            if (checking == '[' || checking == '(' || checking == '{') {
-                word.push(checking);
+
+        // Iterate througth the given expression
+        char chargive[] = expression.toCharArray();
+        for (char name : chargive) {
+            // if current character is close parenthesis ')'
+            if (name == ')') {
+                // pop character is close parenthesis
+                char top = word.peek();
+                word.pop();
+
+                // stores the number of characters between a
+                // closing and opening parenthesis
+                // if this count is less than or equal to 1
+                // then the brackets are redundant else not
+
+                int element = 0;
+                while (top != '(') {
+                    element++;
+                    top = word.peek();
+                    word.pop();
+                }
+                if (element < 1) {
+                    return true;
+                } // push open parenthesis '(', operators and operands to stack
+            } else {
+                word.push(name);
             }
         }
-
+        // No duplicates found
+        return false;
     }
 
     public static void main(String[] args) {
-
+        String word = "(((a+(b))";
+        System.out.println(Duplicate(word));
     }
 }
--- DuplicateParenthesisExpression/README.md
+++ DuplicateParenthesisExpression/README.md
@@ -21,3 +21,13 @@ We can use a stack to solve this problem. The idea is to traverse the given expr
 
 If the current character in the expression is not a closing parenthesis ')', push the character into the stack.
 If the current character in the expression is a closing parenthesis ')', check if the topmost element in the stack is an opening parenthesis or not. If it is an opening parenthesis, then the subexpression ending at the current character is of the form ((exp)); otherwise, continue popping characters from the stack till matching '(' is found for current ')'
+
+
+                                        Approach of solving 
+
+The idea is to use stack. Iterate through the given expression and for each character in the expression.
+ if the character is a open parenthesis ‘(‘ or any of the operators or operands, push it to the top of the stack. 
+ If the character is close parenthesis ‘)’, then pop characters from the stack till matching open parenthesis ‘(‘ is found and a counter is used, whose value is incremented for every character encountered till the opening parenthesis ‘(‘ is found. 
+ If the number of characters encountered between the opening and closing parenthesis pair, which is equal to the value of the counter, is less than 1, then a pair of duplicate parenthesis is found else there is no occurrence of redundant parenthesis pairs. 
+ For example, (((a+b))+c) has duplicate brackets around “a+b”. 
+ When the second “)” after a+b is encountered, the stack contains “((“. Since the top of stack is a opening bracket, it can be concluded that there are duplicate brackets.
