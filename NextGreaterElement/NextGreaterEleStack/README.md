02e457e97878cb29963cb29a86b3c2834c6b1dbf
Elija33 <amponsaeli@gmail.com>
2022-11-28 01:24:42 -0500

    Next Greater Element

--- NextGreaterElement/NextGreaterEleStack/README.md
+++ NextGreaterElement/NextGreaterEleStack/README.md
@@ -12,3 +12,16 @@ array = [2, 5, -3, -4, 6, 7, 2].
 
 Sample Output
 [5, 6, 6, 6, 6, -1, 5]
+
+                                                    Implement the idea:
+The idea is to store the elements for which we have to find the next greater element in a stack and while traversing the array.
+if we find a greater element, we will pair it will the element from the stack till the top element of the stack is less then the current element.
+
+* Push the first element to stack.
+* Pick the rest of the elements one by one and follow the following steps in the loop. 
+    - Mark the current element as next.
+    - If the stack is not empty, compare top most element of stack with next.
+    - If next is greater than the top element, Pop element from the stack. next is the next greater element for the popped element.
+    - Keep popping from the stack while the popped element is smaller than next. next becomes the next greater element for all such popped elements.
+* Finally, push the next in the stack.
+* After the loop in step 2 is over, pop all the elements from the stack and print -1 as the next element for them.   
---
+++ NextGreaterElement/NextGreaterEleStack/StackElement.java
@@ -0,0 +1,69 @@
+package NextGreaterElement.NextGreaterEleStack;
+
+public class StackElement {
+    public static class stack {
+        int top;
+        int items[] = new int[100];
+
+        // Stack functions to be used by print
+        public void push(int number) {
+            if (top == 99) {
+                System.out.println("Stack full");
+            } else {
+                items[++top] = number;
+            }
+        }
+
+        public int pop() {
+            if (top == -1) {
+                System.out.println("Underflow error");
+                return -1;
+            } else {
+                int element = items[top];
+                top--;
+                return element;
+            }
+        }
+
+        public boolean isEmpty() {
+            return (top == -1) ? true : false;
+        }
+    }
+
+    public static void print(int number[]) {
+        int i = 0;
+        stack name = new stack();
+        name.top = -1;
+        int element;
+        int next;
+
+        // push the first element to stack
+        name.push(number[0]);
+
+        // iterate for rest of the elements
+        for (int j = 1; j < number.length; j++) {
+            next = number[i];
+
+            if (name.isEmpty() == false) {
+
+                // if stack is not empty, then
+                // pop an element from stack
+                element = name.pop();
+
+                // if the popped element is smaller than next, then a) print the pair
+                // b) keep popping while elements are smaller and stack is not empty.
+
+                while (element < next) {
+                    System.out.println(element + "-->" + next);
+                    if (name.isEmpty() == true) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
